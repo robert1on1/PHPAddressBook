4e997f8b969d956de674ac13482fa8031e62f598
Kyamanywa Robert <mbabazirobert@gmail.com>
2014-09-03 04:44:25 +0300

    database connection
    database connection strings.

--- index.php
+++ index.php
@@ -3,7 +3,11 @@
  		<title>Address Book</title>
  	</head>
  	<body>
+ 		<?php
+ // Connects to your Database 
+ mysql_connect("YOURINFO.db.5296901.hostedresource.com", "YOURlogin", "YOURpassword") or die(mysql_error()); 
+ mysql_select_db("YOURdatabase") or die(mysql_error()); 
+
 
  	</body> 
  </html> 
- 		
